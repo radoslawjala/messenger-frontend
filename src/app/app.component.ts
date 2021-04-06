ec4305e77bca8f7b06f0c27b29184675139c9d25
radek <marion.cobretti@gmail.com>
2021-04-06 20:39:46 +0200

    działająca stabilna wersja bz wyswietlania wiadomosci w GUI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectorRef, Component} from '@angular/core';
+import { Component, ChangeDetectorRef  } from '@angular/core';
 import {Stomp} from '@stomp/stompjs';
 import * as SockJS from 'sockjs-client';
 import {HttpClient} from '@angular/common/http';
@@ -10,87 +10,119 @@ import {User} from './dto/user';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  user: User = new User();
-
-  users: string[] = [];
-  disabled = true;
+  title = 'habababa';
+  description = 'Angular-WebSocket Demo';
+  user: User;
+  usersList: string[] = [];
+  greetings: string[] = [];
   name: string;
   private stompClient;
   userName: string;
+  message1: string;
+  message2: string;
+  selectedUser: string;
+  textMessage: string;
+  connected: boolean = false;
 
-  constructor(private http: HttpClient, private change: ChangeDetectorRef) {}
+  constructor(private http: HttpClient, private changeDetection: ChangeDetectorRef) { }
 
-  connect(): void {
+
+  connect() {
+    if (this.userName == null || this.userName === "") {
+      alert('Please input a nickname!');
+      return;
+    }
+
+    this.http.post<string>('http://localhost:8080/rest/user-connect',{username: this.userName})
+      .subscribe(data => {
+          console.log(data);
+        },
+        err => {
+          console.error(err);
+        });
+
     const socket = new SockJS('http://localhost:8080/chat');
     this.stompClient = Stomp.over(socket);
     const _this = this;
-    this.stompClient.connect({userName: this.userName}, function () {
-
-      this.stompClient.subscribe('http://localhost:8080/topic/broadcast', function (output) {
-        _this.printInConsole(JSON.parse(output.body));
+    this.stompClient.connect({username: _this.userName}, function (frame) {
+      console.log('Connected: ' + frame);
+      _this.stompClient.subscribe('/topic/broadcast', function (output) {
+        // _this.message1 = JSON.parse(output.body);
+        console.log('wiadomosc z kanalu topic/broadcast: ' + _this.message1);
       });
 
-        // this.stompClient.subscribe('http://localhost:8080/topic/active', function () {
-        //   _this.updateUsers(this.userName);
-        // });
-        //
-        // this.stompClient.subscribe('/user/queue/messages', function (output) {
-        //   _this.printInConsole(JSON.parse(output.body));
-        // });
-        //
-        // _this.sendConnection(' connected to server');
+      _this.stompClient.subscribe('/topic/active', function () {
+        console.log('wiadomosc z topic/active');
+        _this.updateUsers(_this.userName);
+      });
 
+      _this.stompClient.subscribe('/user/queue/messages', function (output) {
+        _this.createTextNode(JSON.parse(output.body));
+      });
+
+      _this.sendConnection(' connected to server');
+      _this.connected = true;
     });
-
-    this.http.post('http://localhost:8080/rest/user-connect', this.userName).subscribe(data => {
-      console.log(data);
-    }, error => {
-      console.error(error);
-    })
-
   }
 
-  disconnect(): void {
-    if(this.stompClient != null) {
-      this.http.post('http://localhost:8080/rest/user-disconnect', this.userName).subscribe(data => {
-          console.log(data);
-        }, error => {
-          console.error(error);
-        }
-      );
+  createTextNode(messageObj: any): void {
+    var _from = messageObj.from;
+    var _recipient = messageObj.recipient;
+    var _text = messageObj.text;
+    var _time = messageObj.time;
 
-      this.stompClient.disconnect(function() {
-        console.log('disconnected');
-      });
+    console.log();
+    console.log();
+    console.log('From: ' + _from + ', recipient: ' + _recipient + ', text: ' + _text + ', time: ' + _time);
+    console.log();
+    console.log();
+  }
+
+  disconnect() {
+    if (this.stompClient != null) {
+      this.sendConnection(' disconnected from server')
+      this.stompClient.disconnect();
     }
+
+    this.connected = false;
+    console.log('Disconnected!');
   }
 
   updateUsers(userName: string): void {
-    this.http.get<string[]>("http://localhost:8080/rest/active-users-except/" + userName).subscribe(data=> {
-      this.users = data;
-      console.log('users: ' + this.users);
-      this.change.detectChanges();
-    }, error => {
-      console.error(error);
-    })
+    this.http.get<string[]>('http://localhost:8080/rest/active-users-except/' + userName).subscribe(data=> {
+        this.usersList = data;
+        // this.usersList = JSON.parse(JSON.stringify(data));
+        console.log('received data from updateUsers: ' + this.usersList);
+        this.changeDetection.detectChanges();
+      },
+      error => {
+        console.error('update user error: ' + error);
+      });
+  }
+
+  showMessage(message: string) {
+    console.log('message: ' + message);
   }
 
   sendConnection(message: string): void {
-    const text = this.userName + message;
-    this.sendBroadcast({'from': 'server', 'text': text});
+    var text = this.userName + message;
+    this.sendBroadcast({'from' : 'server', 'text': text});
 
+    // for first time or last time, list active users:
     this.updateUsers(this.userName);
   }
 
-  sendBroadcast(json: any): void {
-    if(this.stompClient != null) {
-      this.stompClient.send('/app/broadcast', {}, JSON.stringify(json));
-    }
+  sendBroadcast(json) {
+    this.stompClient.send("/app/broadcast", {}, JSON.stringify(json));
   }
 
-  printInConsole(messageObject: any): void {
-    console.log('message: \n' + 'from: ' + messageObject.from + "\n recipient: " + messageObject.recipient
-    + '\n text: ' + messageObject.text + '\n time: ' + messageObject.time);
+  setSelectedUser(userName) {
+    this.selectedUser = userName;
+    console.log('Selected user: ' + this.selectedUser);
+  }
+
+  send(): void {
+    this.stompClient.send("/app/chat", this.userName,
+      JSON.stringify({'from': this.userName, 'text': this.textMessage, 'recipient': this.selectedUser}));
   }
 }
-
